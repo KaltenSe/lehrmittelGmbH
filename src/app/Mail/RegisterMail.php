b6fb952e31f107ad4d7549c0c1eb352a821a2f6b
sebastian <sebastian.kaltenmaier@rsu-reifen.de>
2020-02-13 16:30:31 +0100

    added auth mail-layout

---
+++ src/app/Mail/RegisterMail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class RegisterMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * @var User
+     */
+    protected $user;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from(env('MAIL_AUTH_FROM', 'default'))
+            ->markdown('mails.auth.register', $this->user->toArray());
+    }
+}
---
+++ src/resources/views/mails/auth/register.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Hallo {{$Vorname}} {{$Nachname}}
+
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+<br>
+Viele Grüße
+<br>
+<br>
+<br>
+{{ config('app.name') }}- Team
+@endcomponent
