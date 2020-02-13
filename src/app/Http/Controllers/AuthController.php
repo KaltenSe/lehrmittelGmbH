9b556c14e05438dd6952b8226ec5f3a3552f300d
sebastian <sebastian.kaltenmaier@rsu-reifen.de>
2020-02-13 17:19:11 +0100

    refactor into services

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -2,16 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Mail\RegisterMail;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\Controller;
 use App\Http\Requests\RegistrationFormRequest;
-use App\User;
-use Exception;
-use Illuminate\Http\Response;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Mail;
+use Services\Auth\AuthService;
 
 class AuthController extends Controller
 {
@@ -28,100 +22,61 @@ class AuthController extends Controller
     /**
      * Get a JWT via given credentials.
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @param Request     $request
+     * @param AuthService $authService
+     *
+     * @return JsonResponse
      */
-    public function login(Request $request)
+    public function login(Request $request, AuthService $authService) : JsonResponse
     {
-        #$credentials = request(['email', 'password']);
-
-        $input['password'] = $request->json('data.Passwort');
-
-        if(!empty($request->json('data.Email'))) {
-            $input['Email'] = $request->json('data.Email');
-        } else if(!empty($request->json('data.Loginname'))) {
-            $input['Loginname'] = $request->json('data.Loginname');
-        }
-
-        if (! $token = auth()->attempt($input)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
-
-        return $this->respondWithToken($token);
+        return $authService->login($request);
     }
 
     /**
      * create a user
+     *
+     * @param RegistrationFormRequest $request
+     *
+     * @param AuthService             $authService
+     *
+     * @return JsonResponse
      */
-    public function signUp(RegistrationFormRequest $request) {
+    public function signUp(RegistrationFormRequest $request, AuthService $authService) {
 
-        $user = new User();
-        $user->Vorname = $request->json('data.Vorname');
-        $user->Nachname = $request->json('data.Nachname');
-        $user->Loginname = $request->json('data.Loginname');
-        $user->Nachname = $request->json('data.Nachname');
-        $user->PasswortHash = bcrypt($request->json('data.Passwort'));
-        $user->PLZ = $request->json('data.PLZ');
-        $user->Email = $request->json('data.Email');
-        $user->Adresse = $request->json('data.Adresse');
-
-        try{
-
-            $user->save();
-            Mail::to($user->Email)->send(new RegisterMail($user));
-
-        }catch(Exception $e) {
-            dd($e);
-            return response()->json(['status' => Response::HTTP_BAD_REQUEST, 'data' => ['test'], "message" => 'Es existiert bereit ein Benutzer mit dieser Email/Loginname'], Response::HTTP_BAD_REQUEST);
-        }
-
-        return $this->login($request);
+        return $authService->signUp($request);
     }
 
     /**
      * Get the authenticated User.
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @param AuthService $authService
+     *
+     * @return JsonResponse
      */
-    public function me()
+    public function me(AuthService $authService) : JsonResponse
     {
-        return response()->json(auth()->user());
+        return  $authService->me();
     }
 
     /**
      * Log the user out (Invalidate the token).
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @param AuthService $authService
+     *
+     * @return JsonResponse
      */
-    public function logout()
+    public function logout(AuthService $authService) : JsonResponse
     {
-        auth()->logout();
-
-        return response()->json(['message' => 'Successfully logged out']);
+        return $authService->logout();
     }
 
     /**
      * Refresh a token.
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
-    public function refresh()
+    public function refresh(AuthService $authService) : JsonResponse
     {
-        return $this->respondWithToken(auth()->refresh());
-    }
-
-    /**
-     * Get the token array structure.
-     *
-     * @param  string $token
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function respondWithToken($token)
-    {
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
-        ]);
+        return  $authService->refresh();
     }
 }
