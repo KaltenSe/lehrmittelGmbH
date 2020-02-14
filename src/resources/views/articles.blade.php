64fa1b361a23779ca1ae92c01016486ad3453a78
Philip Simon Kodantke <kodantph@rbs-ulm.lan>
2020-02-14 12:31:49 +0100

    added articles template again

---
+++ src/resources/views/articles.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <form action="{{ route('article') }}" class="form-inline">
+                <div class="form-group">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <label class="input-group-text" for="inputGroupSelectSort">Sortierung</label>
+                        </div>
+                        <select class="custom-select" id="inputGroupSelectSort">
+                            <option selected value="name">Name</option>
+                            <option value="price">Preis</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group mb-3">
+                        <select class="custom-select" id="inputGroupSelectOrder">
+                            <option selected value="asc">aufsteigend</option>
+                            <option value="desc">absteigend</option>
+                        </select>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary">Refresh</button>
+            </form>
+            @foreach ($articles as $article)
+                <div class="card">
+                    <img src="..." class="card-img-left" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $article->name }}</h5>
+                        <p class="card-text">{{ $article->description }}</p>
+                        <a href="{{ route('details') }}" class="btn btn-primary">Details</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+@endsection
