75e6887f312650ede7a20a67546f94727b10b8af
felix357 <felix.schoppe@t-online.de>
2020-02-14 10:28:15 +0100

    Test push via terminal

--- src/resources/views/mails/auth/register.blade.php
+++ src/resources/views/mails/auth/register.blade.php
@@ -1,13 +1,12 @@
 @component('mail::message')
 # Hallo {{$Vorname}} {{$Nachname}}
 
-Sie haben sich erfolgreich mit der E-Mail: {{$Email}}
+Sie haben sich erfolgreich mit der E-Mail: {{$Email}} registriert.
 
 <br>
 Viele Grüße
 <br>
 <br>
-<br/>
 <br>
 {{ config('app.name') }}- Team
 @endcomponent
